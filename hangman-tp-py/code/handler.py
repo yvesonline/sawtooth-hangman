e00555f4d6c015ea9c757b49c3c7eec579649484
Yves Weissig <yves@yvesonline.de>
2020-04-20 19:57:28 +0100

    More bootstrapping, here work on the handler and slight changes in the state

--- hangman-tp-py/code/handler.py
+++ hangman-tp-py/code/handler.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3.5
 # encoding: utf-8
 
-from sawtooth_sdk.processor.handler import TransactionHandler
+import logging
 
-from state import HM_NAMESPACE
+from sawtooth_sdk.processor.handler import TransactionHandler
+from sawtooth_sdk.processor.exceptions import InvalidTransaction
+
+from state import Game, GameState, HmState, HM_NAMESPACE
+from payload import HmPayload
+
+LOGGER = logging.getLogger(__name__)
+MAX_GUESSES = 8
 
 
 class HangmanTransactionHandler(TransactionHandler):
@@ -25,4 +32,67 @@ class HangmanTransactionHandler(TransactionHandler):
     def apply(self, transaction, context):
 
         header = transaction.header
+
         signer = header.signer_public_key
+
+        hm_payload = HmPayload.from_bytes(transaction.payload)
+
+        hm_state = HmState(context)
+
+        if hm_payload.action == "create":
+            game = hm_state.get_game(hm_payload.name)
+            if game:
+                raise InvalidTransaction("Game '{}' already exists".format(hm_payload.name))
+            game = Game(hm_payload.name, hm_payload.guess, "", signer, "")
+            hm_state.set_game(hm_payload.name, game)
+            LOGGER.info("Player '{}' created game ''".format(signer, hm_payload.name))
+        elif hm_payload.action == "delete":
+            try:
+                hm_state.delete_game(hm_payload.name)
+                LOGGER.info("Player '{}' delete game ''".format(signer, hm_payload.name))
+            except KeyError:
+                raise InvalidTransaction("Game '{}' doesn't exist".format(hm_payload.name))
+        elif hm_payload.action == "guess":
+            # Game doesn't exist
+            game = hm_state.get_game(hm_payload.name)
+            if not game:
+                raise InvalidTransaction("Game '{}' doesn't exists".format(hm_payload.name))
+            # Game has ended
+            if game.state != GameState.ongoing:
+                raise InvalidTransaction("Game '{}' has already ended".format(hm_payload.name))
+            # Guess already in hits
+            if hm_payload.guess in game.hits:
+                raise InvalidTransaction("You already guessed '{}' and it was successful".format(hm_payload.guess))
+            # Guess already in misses
+            if hm_payload.guess in game.misses:
+                raise InvalidTransaction("You already guessed '{}' and it was not successful".format(hm_payload.guess))
+            # Compute new game
+            new_misses = game.misses + hm_payload.guess if hm_payload.guess not in game.word else game.misses
+            new_hits = game.hits + hm_payload.guess if hm_payload.guess in game.word else game.hits
+            if set(word) == set(new_hits):
+                state_new = GameState.won
+            elif len(new_misses) > MAX_GUESSES:
+                state_new = GameState.lost
+            else:
+                state_new = GameState.ongoing
+            new_game = Game(
+                game.name,
+                game.word,
+                new_misses,
+                new_hits,
+                game.host,
+                game.guesser,
+                state_new
+            )
+            hm_state.set_game(hm_payload.name, new_game)
+            LOGGER.info("""Game stats:
+                Name: '{}'
+                Word: '{}'
+                Misses: '{}'
+                Hits: '{}'
+                Host: '{}'
+                Guesser: '{}'
+                State: '{}'
+                """.format(game.name, game.word, game.misses, game.hits, game.host, game.guesser, game.state))
+        else:
+            raise InvalidTransaction("Unknown action '{}'".format(hm_payload.action))
--- hangman-tp-py/code/state.py
+++ hangman-tp-py/code/state.py
@@ -3,6 +3,8 @@
 
 import hashlib
 
+from enum import Enum
+
 from cbor2 import dumps, loads
 
 from sawtooth_sdk.processor.exceptions import InternalError
@@ -16,25 +18,43 @@ def _make_hm_address(name):
         hashlib.sha512(name.encode("utf-8")).hexdigest()[:64]
 
 
+class GameState(Enum):
+    ongoing = 1
+    won = 2
+    lost = 3
+
+
 class Game:
-    def __init__(self, name="", word="", misses="", host="", guesser=""):
+    def __init__(self, name="", word="", misses="", hits="", host="", guesser="", state=GameState.ongoing):
         self.name = name
         self.word = word
         self.misses = misses
+        self.hits = hits
         self.host = host
         self.guesser = guesser
+        self.state = state
 
     @classmethod
     def from_dict(cls, d):
-        return cls(d["name"], d["word"], d["misses"], d["host"], d["guesser"])
+        return cls(
+            d["name"],
+            d["word"],
+            d["misses"],
+            d["hits"],
+            d["host"],
+            d["guesser"],
+            d["state"]
+        )
 
     def to_dict(self):
         return {
             "name": self.name,
             "word": self.word,
             "misses": self.misses,
+            "hits": self.hits,
             "host": self.host,
             "guesser": self.guesser,
+            "state": self.state,
         }
 
 
@@ -43,8 +63,12 @@ class HmState:
         self._context = context
 
     def delete_game(self, name):
-        address = _make_hm_address(name)
-        self._context.delete_state([address], timeout=TIMEOUT)
+        game = self.get_game(name)
+        if game:
+            address = _make_hm_address(name)
+            self._context.delete_state([address], timeout=TIMEOUT)
+        else:
+            raise KeyError
 
     def set_game(self, name, game):
         address = _make_hm_address(name)
@@ -64,4 +88,4 @@ class HmState:
             state_de = loads(state_s)
             return Game.from_dict(state_de[-1])
         else:
-            return Game()
+            return None
